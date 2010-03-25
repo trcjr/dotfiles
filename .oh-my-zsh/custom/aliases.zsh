48a746e1f973131ede1234574194825ffc154c9b
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2010-03-24 23:32:59 -0400

    added more aliases and options

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -7,3 +7,43 @@ alias syn4='nohup ~/synergy-1.3.1/synergyc -f -n blackbook 192.168.1.98 &'
 alias cpanp='sudo PERL5LIB="/Users/trcjr/.local/lib/perl5/site_perl/5.10.1" cpanp'
 alias ll='ls -l'
 #alias duhs="/usr/bin/du -sk * | sort -nr | cut -f2 | awk -F "\n" '{print "\""$1"\""}' | xargs /usr/bin/du -sh"
+#
+#
+#
+
+## apt specific
+alias sa='sudo apt-get'
+alias sai='sudo apt-get install'
+alias sau='sudo apt-get update'
+alias saug='sudo apt-get upgrade'
+alias sar='sudo apt-get remove'
+alias saar='sudo apt-get autoremove'
+alias sap='sudo apt-get --purge autoremove'
+alias acp='apt-cache policy'
+alias acs='apt-cache show'
+alias acd='apt-cache depends'
+
+## admin specific
+alias stf='sudo tail -f'
+alias stn='sudo tail -n'
+alias sv='sudo vim'
+
+## play safe
+alias 'rm=rm -i'
+alias 'mv=mv -i'
+alias 'cp=cp -i'
+alias 'mkdir=mkdir -p'
+alias 'dus=du -ms * | sort -n'
+alias 'v=vim'
+alias 'j=jobs -l'
+alias 'tf=tail -f'
+alias 'rh=rehash'
+alias 'q=exit'
+alias 'h=history'
+alias 'gist=pygist.py'
+
+## useful args
+alias -g L='| less'
+alias -g H='| head'
+alias -g S='| sort'
+alias -g T='| tail'
