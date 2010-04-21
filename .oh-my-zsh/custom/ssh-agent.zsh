b3eb1d85a62e1a415452879c146299bf056abd66
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2010-04-20 23:02:27 -0400

    fixed zsh clobber of ssh-agent

--- .oh-my-zsh/custom/ssh-agent.zsh
+++ .oh-my-zsh/custom/ssh-agent.zsh
@@ -4,7 +4,7 @@ SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
      echo "Initialising new SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+     /usr/bin/ssh-agent | sed 's/^echo/#echo/' >| "${SSH_ENV}"
      echo succeeded
      chmod 600 "${SSH_ENV}"
      source "${SSH_ENV}" > /dev/null
