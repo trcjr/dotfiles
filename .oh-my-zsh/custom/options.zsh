74519f6b0dd98434fabc7b39a510c8112ad9564a
Theodore Robert Campbell Jr <trcjr@stupidfoot.com>
2010-07-16 14:52:43 -0400

    merged and added zsh.vim

--- .oh-my-zsh/custom/options.zsh
+++ .oh-my-zsh/custom/options.zsh
@@ -2,6 +2,9 @@
 #setopt AUTO_CONTINUE
 ## don’t write over existing files with >, use >! instead
 setopt NOCLOBBER
+unsetopt SHARE_HISTORY
+#setopt SHARE_HISTORY off
+setopt INCAPPEND_HISTORY
 ## watch other user login/out
 setopt extendedglob
 setopt sh_word_split
